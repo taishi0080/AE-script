8022b4c2fe703d353cbef2cf213c0eba748111a8
taishi0080 <taishi0080@gmail.com>
2017-05-10 10:58:59 +0900

    Add files via upload

---
+++ excell2textLayer.jsx
@@ -0,0 +1,1 @@
+﻿TAB = String.fromCharCode(9);filename = File.openDialog("ファイルを選択してください","");fileObj = new File(filename);if (fileObj.open("r")){while(!fileObj.eof){txt = fileObj.readln();txt = txt.split(TAB);layObj = app.project.activeItem.layers.addText(txt[0]);layObj("position").setValue([txt[1],txt[2],0]);}}
