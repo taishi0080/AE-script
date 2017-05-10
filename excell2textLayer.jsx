1a3e8010478939223deb88a807729baff2dbfd8a
taishi0080 <taishi0080@gmail.com>
2017-05-10 11:15:41 +0900

    Update excell2textLayer.jsx

--- excell2textLayer.jsx
+++ excell2textLayer.jsx
@@ -1,1 +1,13 @@
-﻿TAB = String.fromCharCode(9);filename = File.openDialog("ファイルを選択してください","");fileObj = new File(filename);if (fileObj.open("r")){while(!fileObj.eof){txt = fileObj.readln();txt = txt.split(TAB);layObj = app.project.activeItem.layers.addText(txt[0]);layObj("position").setValue([txt[1],txt[2],0]);}}
+TAB = String.fromCharCode(9);
+filename = File.openDialog("ファイルを選択してください","");
+fileObj = new File(filename);
+if (fileObj.open("r"))
+{
+while(!fileObj.eof)
+{
+txt = fileObj.readln();
+txt = txt.split(TAB);
+layObj = app.project.activeItem.layers.addText(txt[0]);
+layObj("position").setValue([txt[1],txt[2],0]);
+}
+}
